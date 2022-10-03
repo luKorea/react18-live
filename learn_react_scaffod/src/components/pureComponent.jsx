95d5d800793109972daf699dcaeda08dc94a1d72
korealu <643949593@qq.com>
2022-10-03 16:19:31 +0800

    pureComponent学习 2022-10-03 16:19:27

---
+++ learn_react_scaffod/src/components/pureComponent.jsx
@@ -0,0 +1,12 @@
+
+import React, { PureComponent } from 'react'
+
+export class pureComponent extends PureComponent {
+  render() {
+    return (
+      <div>pureComponent 实现SCU性能优化</div>
+    )
+  }
+}
+
+export default pureComponent
