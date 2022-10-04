a9e0843fd4b2050de7cb6e25fc0d089459f8ebdf
korealu <643949593@qq.com>
2022-10-04 10:30:25 +0800

    trna过渡动画学习 2022-10-04 10:30:08

---
+++ learn_react_scaffod/src/components/transition.jsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export class TransitionComponent extends PureComponent {
+  render() {
+    return (
+      <div>TransitionComponent</div>
+    )
+  }
+}
+
+export default TransitionComponent
